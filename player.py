4355a586c0906687649d8d3d459d3e96245c9c8f
Saber AMRI <amrisaber.essai@gmail.com>
2022-04-30 01:24:22 +0200

    add PositifInt to rank

--- player.py
+++ player.py
@@ -1,22 +1,22 @@
 from datetime import date
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt
 
 
 class Gender(Enum):
-    Mail = "H"
+    Male = "H"
     Female = "F"
 
 
 class Player(BaseModel):
-    rank: int
+    rank: PositiveInt
     last_name: str
     birth_date: date
     first_name: str
     gender: Gender
 
 
-player1 = Player(rank=12,
+player1 = Player(rank=-12,
                  last_name="Joe",
                  birth_date="1979-04-23",
                  first_name="Amri",

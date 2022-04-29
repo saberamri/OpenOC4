2343bd34e2765a4030bbb4f7298eca47ba5882aa
Saber AMRI <amrisaber.essai@gmail.com>
2022-04-30 01:47:30 +0200

    add conint to rank

--- player.py
+++ player.py
@@ -1,26 +1,37 @@
-from datetime import date
+from datetime import date, timedelta
 from enum import Enum
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, PositiveInt, conint, constr
 
 
 class Gender(Enum):
+    """
+    Gender is a class that inherits from the Enum class.
+    Enum is a basic python module integrated into the language 
+    it will enumerate the values of the gender variable.
+
+    """
     Male = "H"
     Female = "F"
 
 
 class Player(BaseModel):
+    """
+    The Player() class inherits from the BaseModel() class
+    This class contains the attributes and methods of the player user.
+
+    """
     id: PositiveInt
-    rank: PositiveInt
-    last_name: str
+    rank: conint(strict=True, gt=0, le=3000)
+    last_name: constr(strict=True, min_length=2, max_length=12)
     birth_date: date
     first_name: str
     gender: Gender
 
 
 player1 = Player(id=42,
-                 rank=12,
+                 rank=30,
                  last_name="Joe",
-                 birth_date="1979-04-23",
+                 birth_date="2020-04-23",
                  first_name="Amri",
                  gender="H")
 print(player1)

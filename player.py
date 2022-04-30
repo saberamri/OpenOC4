7873ed3a99e62c086f681e0934dd25d8346ea9e6
Saber AMRI <amrisaber.essai@gmail.com>
2022-04-30 02:10:43 +0200

    add constr to first_name and create validator to birth_date

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from datetime import date, timedelta
 from enum import Enum
-from pydantic import BaseModel, PositiveInt, conint, constr
+from pydantic import BaseModel, PositiveInt, conint, constr, validator
 
 
 class Gender(Enum):
@@ -14,6 +14,9 @@ class Gender(Enum):
     Female = "F"
 
 
+
+
+
 class Player(BaseModel):
     """
     The Player() class inherits from the BaseModel() class
@@ -24,14 +27,30 @@ class Player(BaseModel):
     rank: conint(strict=True, gt=0, le=3000)
     last_name: constr(strict=True, min_length=2, max_length=12)
     birth_date: date
-    first_name: str
+    first_name: constr(strict=True, min_length=2, max_length=12)
     gender: Gender
+
+
+    @validator("birth_date")
+    def check_age(cls, v):
+        """verify that participants are at least 18 years old.
+            Args:
+                v (date): date of birth of participants.
+            Raises:
+                ValueError: the age of the participants is under 18.
+            Returns:
+                v: birth date of participant.
+        """
+        age = (date.today() - v) // timedelta(days=365.2425)
+        if age < 18:
+            raise ValueError('age must be > 18')
+        return v
 
 
 player1 = Player(id=42,
                  rank=30,
                  last_name="Joe",
-                 birth_date="2020-04-23",
+                 birth_date="2001-04-23",
                  first_name="Amri",
                  gender="H")
 print(player1)

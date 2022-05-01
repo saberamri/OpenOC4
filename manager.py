86b6f84bfe42b35ea964cf2a04284ad2dbb6e42a
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-01 09:43:05 +0200

    instantiate an dictionary -items- to store the loaded data

--- manager.py
+++ manager.py
@@ -9,12 +9,11 @@ class Manager:
 
     def __init__(self, item_type: Any):
         self.item_type = item_type
+        self.items = {}
         self.load_from_jason()
 
     def load_from_jason(self, path: str):
         with open(path) as json_data:
             data_dict = json.load(json_data)
-            ls = []
             for item_data in data_dict:
                 item = Player(**item_data)
-                ls.append(item)

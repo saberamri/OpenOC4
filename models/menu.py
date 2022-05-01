af834363f0f71e14e2e83551d074e38d4848da07
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-01 19:29:35 +0200

    add docstring to Menu() and View()

--- models/menu.py
+++ models/menu.py
@@ -3,6 +3,19 @@ from view import View
 
 
 class Menu(View):
+    """The Menu class contains two attrributes:
+    - title
+    - options
+    and contain a show class method overloaded from the parent class view().
+
+    Args:
+        View (class): parent class
+    """
     def __init__(self, title: str, options: List[str]):
+        """constructor
+        Args:
+            title (str): title
+            options (List[str]): options list
+        """
         content = "\n".join(options)
         super().__init__(title=title, content=content)
--- view.py
+++ view.py
@@ -1,9 +1,22 @@
 class View:
+    """
+    view contains two attributes title and content and a show class method to display them.
+    """
     def __init__(self, title: str, content: str):
+        """constructor
+        Args:
+            title (str): capitalized view title
+            content (str): view content: allow empty content
+            is_blocking (bool): blocking parameter: stop display and 
+        """
         self.title = title
         self.content = content
 
     def show(self):
+        """
+        - display of title and content separated by a line
+        - console cls after each display
+        """
         print(self.title)
         print("-" * len(self.title))
         print(self.content)

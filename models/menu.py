c9f53bed0c350c82d0698da7b823e5ba3aa866a8
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-01 17:32:46 +0200

    display title and options of Menu()

--- models/menu.py
+++ models/menu.py
@@ -4,6 +4,5 @@ from view import View
 
 class Menu(View):
     def __init__(self, title: str, options: List[str]):
-        self.options = options
         content = "\n".join(options)
         super().__init__(title=title, content=content)
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-from view import View
+from models.menu import Menu
 
+# view = View(title="title 1",
+#             content= "content1")
+# print(view.show())
 
-view = View(title="Bienvenue sur chess manager version 1.0",
-            content=["Gérer les joueurs", "Gérer les tournois"])
-
-print(view.show())
+Menu(title="Bienvenue sur chess manager version 1.0",
+     options=["Gérer les joueurs", "Gérer les tournois"]).show()

4eb86e35273f1d4b4d5ec21d0db2132062367070
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-01 11:13:10 +0200

    fix main to load json data

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from models.player import Player
 
 player_manager = Manager(item_type=Player)
 player_manager.load_from_jason("./Jason/players.json")
-print(player_manager)
+print(player_manager.items)

6483844416e7e719519d61bc0f96ba675b068a7a
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-01 11:10:08 +0200

    add main to execute manager

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 from manager import Manager
+from models.player import Player
+
+
+player_manager = Manager(item_type=Player)
+player_manager.load_from_jason("./Jason/players.json")
+print(player_manager)
--- manager.py
+++ manager.py
@@ -10,11 +10,10 @@ class Manager:
     def __init__(self, item_type: Any):
         self.item_type = item_type
         self.items = {}
-        self.load_from_jason()
 
     def load_from_jason(self, path: str):
         with open(path) as json_data:
             data_dict = json.load(json_data)
             for item_data in data_dict:
                 item = self.item_type(**item_data)
-                item = self.items[item.id]
+                self.items[item.id] = item

5d7b6c0599b5bcfcbcac191e9d72433fe34e8614
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-04 00:14:22 +0200

    override the show() method which displays the title and not the content

--- form.py
+++ form.py
@@ -6,3 +6,6 @@ class Form(View):
     def __init__(self, title: str, fields: List[str]):
         super().__init__(title=title)
         self.fields = fields
+
+    def show():
+        super().show()

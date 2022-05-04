b5edef72b8908794b577f1441a36c0315f4b6444
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-04 10:23:14 +0200

    concatenate the contents of -Table()- in the form of row and column generation and add the two functions -gen_headers()- and -gen_lines()-

--- table.py
+++ table.py
@@ -8,6 +8,25 @@ class Table(View):
         View (class): parent class
     """
     def __init__(self, title: str, cols: List[str], items: List[Any]):
-        super().__init__(title)
+        content = self.gen_headers(cols) + self.gen_lines(items)
+        super().__init__(title=title, content=content)
         self.cols = cols
         self.items = items
+
+    def gen_headers(self, cols: List[str]):
+        """generate headers
+        Args:
+            cols (List[str]): headers
+        Returns:
+            headers: columns name
+        """
+        pass
+
+    def gen_lines(self, items: List[Any]):
+        """generate lines
+        Args:
+            items (List[Any]): list of items
+        Returns:
+            lines: data lines
+        """
+        pass

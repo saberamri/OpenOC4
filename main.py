581864b477ec1e29a93f707f54bbd1a6d4100477
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-06 09:24:58 +0200

    add main menu class -MainMenu()-: display title and options

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from menu import Menu
+from main_menu import MainMenu
+from views.menu import Menu
 
 
 def main():
     """principal controller
     """
-    Menu(title="Bienvenue sur chess Manager version 1.0",
-         options=["Gérer les joueurs", "Gérer les tournois"]).show()
+    # Menu(title="Bienvenue sur chess Manager version 1.0",
+    #      options=["Gérer les joueurs", "Gérer les tournois"]).show()
+    MainMenu().show()
 
 
 main()
---
+++ main_menu.py
@@ -0,0 +1,7 @@
+from views.menu import Menu
+
+
+class MainMenu(Menu):
+    def __init__(self):
+        super().__init__(title="Bienvenue sur chess Manager version 1.0",
+                     options=["Gérer les joueurs", "Gérer les tournois"])

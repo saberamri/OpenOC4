711416ebb387a465f4edb9b774d9aa6e2b674080
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-07 08:57:43 +0200

    Creation of a choice variable to manage menu choices: -PlayerMenu()- and -TournamentMenu()-

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from main_menu import MainMenu
+from player_menu import PlayerMenu
 from views.menu import Menu
 
 
@@ -8,6 +9,12 @@ def main():
     # Menu(title="Bienvenue sur chess Manager version 1.0",
     #      options=["Gérer les joueurs", "Gérer les tournois"]).show()
     choice = MainMenu().show()
-
+    match choice:
+        case 1:
+            PlayerMenu().show()
+        case 2:
+            TournamentMenu().show()
+        # case _:
+        #     quit()
 
 main()

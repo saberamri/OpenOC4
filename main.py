b458459ee1d96d9856c34d8750767e0738cc02f9
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-07 13:13:31 +0200

    Add choice 1 -add_players()- in Menu() -PlayerMenu()-

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ def tournaments():
 def main():
     """principal controller
     """
-    # Menu(title="Bienvenue sur chess Manager version 1.0",
-    #      options=["Gérer les joueurs", "Gérer les tournois"]).show()
     choice = MainMenu().show()
     match choice:
         case 1:

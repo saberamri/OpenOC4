7ac15cc5cdc57fae756877f9f760c50392e2c7dd
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-07 12:44:00 +0200

    Test the first form to add a player in the -main()-

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from form import Form
+from views.form import Form
 from manager import Manager
-from menu import Menu
+from views.menu import Menu
 from models.player import Player
-from table import Table
+from views.table import Table
 
 # view = View(title="title 1",
 #             content= "content1")
@@ -17,14 +17,23 @@ from table import Table
 #                                         ("sexe", "gender", str),
 #                                         ("classement", "rank", int)]).show())
 
-player_manager = Manager(item_type=Player)
-player_manager.load_from_jason("./Jason/players.json")
+# player_manager = Manager(item_type=Player)
+# player_manager.load_from_jason("./Jason/players.json")
 
-print(Table(title="liste des joueurs",
-      cols=[("id", "id"),
-            ("classement", "rank"),
-            ("nom", "last_name"),
-            ("date de naissance", "birth_date"),
-            ("prénom", "first_name"),
-            ("sexe", "gender")],
-      items=player_manager.all()).show())
+# print(Table(title="liste des joueurs",
+#       cols=[("id", "id"),
+#             ("classement", "rank"),
+#             ("nom", "last_name"),
+#             ("date de naissance", "birth_date"),
+#             ("prénom", "first_name"),
+#             ("sexe", "gender")],
+#       items=player_manager.all()).show())
+
+Form(title="Ajouter un joueur",
+     fields=[("Nom", "last_name", str),
+             ("Prénom", "first_name", str),
+             ("année de naissance", "birth_year", int),
+             ("mois de naissance", "birth_month", int),
+             ("jour de naisance", "birth_day", int),
+             ("sexe", "gender", str),
+             ("classement", "rank", int)]).show()

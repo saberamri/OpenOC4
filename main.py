e43221188e21bc2a4b1505bd41d3d83054647b0d
Saber AMRI <amrisaber.essai@gmail.com>
2022-05-07 13:24:35 +0200

    Create an -add_players()- function that just stores data in a -form_data- dict and returns to the previous menu -PlayerMenu()-

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from main_menu import MainMenu
+from player_form import PlayerForm
 from player_menu import PlayerMenu
 from tournament_menu import TournmanetMenu
 
@@ -6,8 +7,15 @@ from tournament_menu import TournmanetMenu
 def players():
     choice = PlayerMenu().show()
     match choice:
+        case 1:
+            add_players()
         case 5:
             main()
+
+
+def add_players():
+    form_data = PlayerForm().show()
+    players()
 
 
 def tournaments():
